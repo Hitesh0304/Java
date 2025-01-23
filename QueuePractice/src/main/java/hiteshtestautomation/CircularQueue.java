11b1fea891da42c1bf4b91c29c35a2f75abc5e48
Hitesh Moudgil <200378502@student.georgianc.on.ca>
2025-01-23 01:17:44 -0500

    added circular queue class

---
+++ QueuePractice/src/main/java/hiteshtestautomation/CircularQueue.java
@@ -0,0 +1,59 @@
+package hiteshtestautomation;
+
+public class CircularQueue {
+    private int[] arr;
+    int rear;
+    int front;
+    int size = 0;
+    int capacity;
+
+    public CircularQueue(int capacity) {
+        arr = new int[capacity];
+        rear = 0;
+        front = 0;
+        size = 0;
+        this.capacity = capacity;
+    }
+
+    public int enqueue(int num) {
+        if(isFull()) {
+            throw new IndexOutOfBoundsException("Queue is full");
+        }
+        else {
+            arr[rear] = num;
+            rear++;
+            size++;
+        }
+        return num;
+    }
+
+    public int dequeue() {
+        int numRemoved;
+        if(isEmpty()) {
+            throw new IndexOutOfBoundsException("Queue is empty.");
+        }
+        else {
+            numRemoved = arr[front];
+            front = front + 1;
+            size--;
+        }
+        return numRemoved;
+    }
+
+    public int peek() {
+        if(isEmpty()) {
+            throw new IndexOutOfBoundsException("Queue is empty.");
+        }
+        else {
+            return arr[front];
+        }
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public boolean isFull(){
+        return size == capacity;
+    }
+}
