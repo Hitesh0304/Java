f7c73239cb3124d6b8e4ffcc0a67f9ce175c45b5
Hitesh Moudgil <200378502@student.georgianc.on.ca>
2025-01-25 18:37:54 -0500

    added methods to AStack.java

--- StackPractice/src/main/java/hiteshtestautomation/AStack.java
+++ StackPractice/src/main/java/hiteshtestautomation/AStack.java
@@ -5,10 +5,44 @@ public class AStack {
     private int bottom;
     private int capacity;
     private int[] arr;
+    private int size;
 
     public AStack(int capacity) {
         top = 0;
         bottom = 0;
         this.capacity = capacity;
+        size = 0;
+        arr = new int[capacity];
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void enqueue(int x) {
+        if (isFull()) {
+            throw new IllegalStateException("Queue is full.");
+        }
+        arr[bottom] = x;
+        top++;
+        size++;
+    }
+
+    public void dequeue(int x) {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty.");
+        }
+        for(int i =0; i < arr.length;i++) {
+            //TO DO
+        }
+    }
+
+
+    public boolean isEmpty() {
+        return getSize() == 0;
+    }
+
+    public boolean isFull() {
+        return getSize() == capacity;
     }
 }
---
+++ StackPractice/src/main/java/hiteshtestautomation/Main.java
@@ -0,0 +1,8 @@
+package hiteshtestautomation;
+
+public class Main {
+    public static void main(String []args) {
+        AStack aStack = new AStack(5);
+
+    }
+}
