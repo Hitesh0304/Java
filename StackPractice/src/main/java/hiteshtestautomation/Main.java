31b7bcb4c6a1d15f1f1b4d038279629abd3cf69b
Hitesh Moudgil <200378502@student.georgianc.on.ca>
2025-01-25 21:18:13 -0500

    updated Main.java

--- StackPractice/src/main/java/hiteshtestautomation/Main.java
+++ StackPractice/src/main/java/hiteshtestautomation/Main.java
@@ -5,6 +5,13 @@ public class Main {
         AStack aStack = new AStack(5);
 
         PerformanceAStack performanceAStack = new PerformanceAStack();
+        /*
+        Performance result:
+        Time taken for AStack enqueue operation: 0
+        MilliSeconds taken for AStack enqueue operation: 3
+        Seconds taken for AStack dequeue operation: 0
+        MilliSeconds taken for AStack dequeue operation: 3
+         */
         performanceAStack.enqueuePerformance();
         performanceAStack.dequeuePerformance();
     }
